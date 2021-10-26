3ea9e19c642b4d4510e79aadb03a77bc31192fef
JakeCodes <jakethesnake20006@gmail.com>
2021-10-25 21:18:10 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Christmas List</h1>
+  <List msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    List
   }
 }
 </script>
@@ -19,7 +19,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="list">
+    <ul>
+      <li>GPS Running Watch</li>
+      <li>Running Shoes</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<style scoped>
+  ul {
+    text-align: left;
+  }
+</style>
