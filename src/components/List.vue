7ad38ad2e2a4a954d52f9b4ffd3b7e9ceffaced1
JakeCodes <jakethesnake20006@gmail.com>
2021-10-26 08:40:01 -0500

    changed list

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,9 +3,7 @@
     <ul>
       <li>GPS Running Watch 
         <br>
-        -<a target="_blank" href="https://www.amazon.com/PACE-Silicone-Barometer-Connections-Wearable4U/dp/B08PZDWP57/ref=mp_s_a_1_1_sspa?adgrpid=107210782779&dchild=1&gclid=CjwKCAjwq9mLBhB2EiwAuYdMtSQbdOkb0MTIKDHg7F4zIhk3C8wBfVHmV78DrFxQkk7NCbk2TeHuyRoCEPgQAvD_BwE&hvadid=457246771422&hvdev=m&hvlocphy=9027821&hvnetw=g&hvqmt=e&hvrand=3752404182634490714&hvtargid=kwd-901360053288&hydadcr=25568_11240464&keywords=coros%2Bpace%2B2&qid=1635178302&sr=8-1-spons&psc=1&smid=AY1YC2BWA3QO7&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExT0FKT1JIMlNPQUpNJmVuY3J5cHRlZElkPUEwMDUzNjQwMUtBVDBWT0VRSFE0OCZlbmNyeXB0ZWRBZElkPUEwNjgxMjUxMTRLOVJCWVJBVDBVUyZ3aWRnZXROYW1lPXNwX3Bob25lX3NlYXJjaF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl&th=1">Coros Pace 2</a>
-        <br>
-        -<a target="_blank" href="https://www.amazon.com/Garmin-Forerunner-Running-Watch-Black/dp/B0160BC1FO/ref=mp_s_a_1_2_sspa?adgrpid=107210782779&dchild=1&gclid=CjwKCAjwq9mLBhB2EiwAuYdMtSQbdOkb0MTIKDHg7F4zIhk3C8wBfVHmV78DrFxQkk7NCbk2TeHuyRoCEPgQAvD_BwE&hvadid=457246771422&hvdev=m&hvlocphy=9027821&hvnetw=g&hvqmt=e&hvrand=3752404182634490714&hvtargid=kwd-901360053288&hydadcr=25568_11240464&keywords=coros+pace+2&qid=1635178550&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQVVFTUw0Nkg3RlJYJmVuY3J5cHRlZElkPUEwNDg4MTg1MTZNUlJEVkRYUE1INiZlbmNyeXB0ZWRBZElkPUEwMzgxNTQwMVZGSVNNSDIxWlkzSiZ3aWRnZXROYW1lPXNwX3Bob25lX3NlYXJjaF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl">Garmin Forerunner 235</a>
+        -<a target="_blank" href="https://www.amazon.com/dp/B08GNV86NR?tag=versus0e-20&linkCode=ogi&th=1">Coros Pace 2</a>
       </li>
       <li>Running Shoes</li>
       -<a target="_blank" href="https://www.saucony.com/en/endorphin-speed-2/50612M.html?dwvar_50612M_color=S20688-116#cgid=mens-running&prefn1=isOnSale&prefv1=false&start=1">Saucony Endorphin Speed 2</a>
