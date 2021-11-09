fe893b68ebf93a62d2ffc7f798484977db5530be
JakeCodes <jakethesnake20006@gmail.com>
2021-11-08 18:41:35 -0600

    added more to list

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,10 +3,27 @@
     <ul>
       <li>GPS Running Watch 
         <br>
-        -<a target="_blank" href="https://www.amazon.com/dp/B08GNV86NR?tag=versus0e-20&linkCode=ogi&th=1">Coros Pace 2</a>
+
+        -<a target="_blank" href="https://www.amazon.com/dp/B08GNV86NR?tag=versus0e-20&linkCode=ogi&th=1">
+        Coros Pace 2
+        </a>
       </li>
+
       <li>Running Shoes</li>
-      -<a target="_blank" href="https://www.saucony.com/en/endorphin-speed-2/50612M.html?dwvar_50612M_color=S20688-116#cgid=mens-running&prefn1=isOnSale&prefv1=false&start=1">Saucony Endorphin Speed 2</a>
+      -<a target="_blank" href="https://www.saucony.com/en/endorphin-speed-2/50612M.html?dwvar_50612M_color=S20688-116#cgid=mens-running&prefn1=isOnSale&prefv1=false&start=1">
+      Saucony Endorphin Speed 2
+      </a>
+
+      <li>Clothes</li>
+      -<a target="_blank" href="https://www.boausa.com/collections/short-long-sleeve-shirts/products/mens-primo-regular-fit-premocrew-short-sleeve-charcoal">
+      Charcoal Primo Regular Fit Crew
+        </a>
+
+      <br>
+
+      -<a target="_blank" href="https://www.brooksrunning.com/en_us/mens-sherpa-7inch-running-shorts-with-liner/211334.html">
+      Running shorts
+      </a>
     </ul>
   </div>
 </template>
